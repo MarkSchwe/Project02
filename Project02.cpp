d9088eedbe1b2aff6c4fe322b0d62b689e62fb5a
MarkSchwe <91101294+MarkSchwe@users.noreply.github.com>
2021-11-14 21:08:13 -0500

    Update Project02.cpp

--- Project02.cpp
+++ Project02.cpp
@@ -6,11 +6,58 @@ using namespace std;
 
 
 int main(){
+//miniDList<int>test;
+//miniListStack<int> test2;
+//miniArrStack<int> test3;
+miniArrQueue<int> test4;
+//miniListQueue<int> test5;
+/*
+for(int i = 0; i < 5; i++){
+    test.addFront(i);
+}
+test.display();
+test.removeBack();
+test.removeFront();
+test.display();
+cout << test.size() << endl;
+cout << test.contains(10) << endl;
+cout << test.back() << endl;
+cout << test.front() << endl;
+*/
 
 
+/*
+for(int i = 0; i < 5; i++){
+    test2.push(i);
+}
+test2.printStack();
+test2.pop();
+test2.printStack();
+*/
 
+/*
+for(int i = 0; i < 5; i++){
+    test3.push(i);
+}
+test3.printStack();
+test3.pop();
+test3.printStack();
+*/
 
+for(int i = 0; i < 5; i++){
+    test4.enqueue(i);
+}
+test4.printQueue();
+test4.dequeue();
+test4.printQueue();
 
-
+/*
+for(int i = 0; i < 5; i++){
+    test5.enqueue(i);
+}
+test5.printQueue();
+test5.dequeue();
+test5.printQueue();
+*/
 return 0;
 }
