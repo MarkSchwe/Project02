af0cd29603ab4a2e7d460d49408d5647bc58ffed
MarkSchwe <91101294+MarkSchwe@users.noreply.github.com>
2021-11-14 21:11:17 -0500

    Update miniDList.h

--- miniDList.h
+++ miniDList.h
@@ -1,5 +1,7 @@
 #ifndef miniDList_h
 #define miniDList_h
+#include <iostream>
+using namespace std;
 template <class DataType>
 class miniDList{
 private:
@@ -34,5 +36,156 @@ protected: //local utilities
 void add(DNode * v, const DataType& e); //insert new node before v
 void remove(DNode* v); //remove node v
 };
+
+template <class DataType>
+miniDList<DataType>::miniDList(const miniDList& l){
+DNode *copy = l->header->next;
+while(copy != l->trailer){
+addFront(copy->data);
+}
+} // Copy constructor
+
+template <class DataType>
+miniDList<DataType>::~miniDList(){
+DNode *test = header;
+while(test->next != trailer){
+    removeBack();
+}
+test = nullptr;
+} // Destructor
+
+template <class DataType>
+const DataType& miniDList<DataType>::front() const{
+    return header->next->data;
+} // get front element
+
+template <class DataType>
+const DataType& miniDList<DataType>::back() const{
+    return trailer->prev->data;
+} // get back element
+
+template <class DataType>
+void miniDList<DataType>::removeFront(){
+DNode *delNode = header->next;
+DNode *link = delNode->next;
+header->next = link;
+link->prev = header;
+delete delNode;
+} // Remove & return the front
+
+template <class DataType>
+void miniDList<DataType>::removeBack(){
+DNode *delNode = trailer->prev;
+DNode *link = delNode->prev;
+link->next = trailer;
+trailer->prev = link;
+delete delNode;
+} // Remove & return the back 
+
+template <class DataType>
+void miniDList<DataType>::addFront(const DataType& e){
+DNode *insert = new DNode;
+insert->data = e;
+DNode *after = header->next;
+after->prev = insert;
+insert->next = after;
+insert->prev = header;
+header->next = insert;
+} // Add to the front
+
+template <class DataType>
+void miniDList<DataType>::addBack(const DataType& e){
+DNode *insert = new DNode;
+insert->data = e;
+DNode *before = trailer->prev;
+before->next = insert;
+trailer->prev = insert;
+insert->next = trailer;
+insert->prev = before;
+} // Add to the back
+
+template <class DataType>
+int miniDList<DataType>::size() const{
+DNode *Test = header;
+int listSize = 0;
+Test = Test->next;
+while (Test != trailer){
+    listSize++;
+    Test = Test->next;
+}
+Test = nullptr;
+return listSize;
+} // Returns the number of elements in list
+
+template <class DataType>
+bool miniDList<DataType>::contains(const DataType& e){
+DNode *Test = header;
+while (Test != trailer && Test->data != e){
+    Test = Test->next;
+}
+if(Test->data == e){
+    return true;
+} else {
+    return false;
+}
+Test = nullptr;
+} // Tests for membership
+
+template <class DataType>
+void miniDList<DataType>::display() const{
+DNode *Test = header->next;
+while (Test != trailer){
+    cout << "Data: " << Test->data << endl;
+    Test = Test->next;
+}
+Test = nullptr;
+} // Prints the elements of list
+
+template <class DataType>
+miniDList<DataType>& miniDList<DataType>::operator=(const miniDList<DataType>& l){
+while(header->next != trailer){
+removeFront(header->next);
+}
+DNode *copy = l->header->next;
+while(copy != l->trailer){
+addFront(copy->data);
+}
+} //Overloaded assignment
+
+template <class DataType>
+void miniDList<DataType>::add(DNode * v, const DataType& e){
+DNode *insert = new DNode;
+insert->data = e;
+DNode *search = header;
+while(search->next != v && search->next != trailer){
+    search = search->next;
+}
+if(search->next == v){
+    insert->next = v;
+    insert->prev = search;
+    search->next = insert;
+    v->prev = insert;
+} else {
+    cout << "insert location not found." << endl;
+}
+} //insert new node before v
+
+template<class DataType>
+void miniDList<DataType>::remove(DNode* v){
+DNode *delNode = header;
+DNode *after, *before;
+while(delNode != v && delNode != trailer){
+delNode = delNode->next;
+} 
+if(delNode == v){
+after = delNode->next;
+before = delNode->prev;
+after->prev = before;
+before->next = after;
+delete delNode;
+} else {
+    cout << "node for deletion not found." << endl;
+}
+} //remove node v
 
 #endif
